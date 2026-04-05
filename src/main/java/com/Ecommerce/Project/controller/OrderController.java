4aba9d8b17362a3abd5e683c27f27fbadd49478c
pavan <boinipavankumar121@gmail.com>
2026-04-05 10:40:04 +0530

    inventory-locking implemented and tested with 100 concurrent threads

--- src/main/java/com/Ecommerce/Project/controller/OrderController.java
+++ src/main/java/com/Ecommerce/Project/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.Ecommerce.Project.controller;
 
 import com.Ecommerce.Project.DTO.OrderDTO;
 import com.Ecommerce.Project.service.OrderService;
+import org.hibernate.dialect.lock.OptimisticEntityLockException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +28,15 @@ public class OrderController {
             orderService.saveOrder(orderDTO);
             return ResponseEntity.ok("order accepted");
         } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @PostMapping("/singleOrder")
+    public ResponseEntity<String> placeOrder(@RequestBody OrderDTO orderDTO){
+        try{
+            orderService.placeOrder(orderDTO);
+            return ResponseEntity.ok("Order Placed Successfully");
+        } catch (OptimisticEntityLockException e) {
             throw new RuntimeException(e);
         }
     }
--- src/main/java/com/Ecommerce/Project/service/OrderServiceImpl.java
+++ src/main/java/com/Ecommerce/Project/service/OrderServiceImpl.java
@@ -8,6 +8,7 @@ import com.Ecommerce.Project.Entity.Order;
 import com.Ecommerce.Project.Entity.Product;
 import com.Ecommerce.Project.Entity.User;
 import jakarta.transaction.Transactional;
+import org.hibernate.dialect.lock.OptimisticEntityLockException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +31,7 @@ public class OrderServiceImpl implements OrderService{
 
     @Override
     public void saveOrder(List<OrderDTO> orderDTO) {
+
         for(OrderDTO tempOrderDTO :orderDTO)
         {
             int price=productDAO.getProductPrice(tempOrderDTO.productId);
@@ -56,5 +58,33 @@ public class OrderServiceImpl implements OrderService{
     @Transactional
     public void deleteOrder(Integer id) {
         orderDAO.deleteOrder(id);
+    }
+
+    @Override
+    @Transactional
+    public void placeOrder(OrderDTO orderDTO) {
+
+            try {
+                Product product = productDAO.getProductById(orderDTO.productId);
+                User user = userDAO.getUser(orderDTO.getUserId());
+                Order order = new Order();
+                order.setUser(user);
+                order.setStatus(Order.Status.PLACED);
+                order.setQuantity(orderDTO.getQuantity());
+                order.setLocalDateTime(LocalDateTime.now());
+                order.setTotalPrice(product.getPrice() * orderDTO.totalPrice);
+                orderDAO.saveOrder(order);
+                product.setStockAvailable(product.getStockAvailable() - orderDTO.quantity);
+                Thread.sleep(30000);
+                productDAO.saveProduct(product);
+            } catch (OptimisticEntityLockException e) {
+
+                    throw new OptimisticEntityLockException( e,"concurreny issue occured retry");
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+
     }
 }
