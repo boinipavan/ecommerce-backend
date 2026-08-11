31df8be41796403637efa1d211a0304ced4a996f
pavan <boinipavankumar121@gmail.com>
2026-08-11 22:57:24 +0530

    test: improve integration testing with Testcontainers

--- src/test/java/com/Ecommerce/Project/config/AbstractIntegrationTest.java
+++ src/test/java/com/Ecommerce/Project/config/AbstractIntegrationTest.java
@@ -2,17 +2,15 @@ package com.Ecommerce.Project.config;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@Testcontainers
 @ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
 public abstract class AbstractIntegrationTest {
 
-    @Container
-    @ServiceConnection
-    protected static final MySQLContainer<?> mySQLContainer=new MySQLContainer<>("mysql:8.4");
 }
--- src/test/java/com/Ecommerce/Project/integration/ContextLoadTest.java
+++ src/test/java/com/Ecommerce/Project/integration/ContextLoadTest.java
@@ -2,9 +2,7 @@ package com.Ecommerce.Project.integration;
 
 import com.Ecommerce.Project.config.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 public class ContextLoadTest extends AbstractIntegrationTest {
 
     @Test
